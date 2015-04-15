13402aea6d799e97eb7f69c9db2d97812f293ba4
Andrinux <andrinux@users.noreply.github.com>
2015-04-15 18:52:45 -0400

    Update shellSort.c

--- Cracking/shellSort.c
+++ Cracking/shellSort.c
@@ -14,41 +14,13 @@ int main()
 	return 1;
 }
 
-/*
-void shellSort(int v[], int len)
-{
-	int step;
-	int col, row, i, j;
-	int tmp;
-	for (step = len / 2; step > 0; step /= 2){
-		//For each column, do an insertion sorting
-		for (col = 0; col < step; col++){
-			//Begin insert sort, the data to be sorted is:
-			//col+step*1, col+step*2, col+step*3...
-			for (row = 1; row <= (len -1 - col) / step; row++){
-				tmp = v[col + row*step]; //Value to be placed
-				for (i = 0; i < row; i++){//Search row 0 to cur row
-					if (v[col + i*step] > tmp){//If some row bigger than cur, shift down one by one
-						for (j = row - 1; j >=i; j--){
-							v[(j+1)*step+col] = v[j*step+col];
-						}
-						v[col+step*i] = tmp;
-					}
-				}
-			}
-		}
-	}
 
-	for (int i = 0; i < len; i++)
-		printf("%d, ",v[i]);
-}
-*/
 void shellSort(int a[], int n)
 {
 	int i, j, gap;
 
-	for (gap = n / 2; gap > 0; gap /= 2) //²½³¤  
-	for (i = 0; i < gap; i++)        //Ö±½Ó²åÈëÅÅÐò  
+	for (gap = n / 2; gap > 0; gap /= 2) //Â²Â½Â³Â¤  
+	for (i = 0; i < gap; i++)        //Ã–Â±Â½Ã“Â²Ã¥ÃˆÃ«Ã…Ã…ÃÃ²  
 	{
 		for (j = i + gap; j < n; j += gap)
 			if (a[j] < a[j - gap])
