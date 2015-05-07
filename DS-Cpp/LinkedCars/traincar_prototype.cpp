4eff79265166faffd5fd4917e973490a4558e547
Andrinux Loid <andrinux.loid@gmail.com>
2015-05-07 12:00:37 -0400

    implement Push back

--- DS-Cpp/LinkedCars/traincar_prototype.cpp
+++ DS-Cpp/LinkedCars/traincar_prototype.cpp
@@ -5,6 +5,26 @@
 
 #include "traincar_prototypes.h"
 
-void PushBack(TrainCar* simple, static TrainCar* newCar){
+//Impelement the pushback function, "simple" is the linked list head.
+//Note: No need to add "static" here 
+void PushBack(TrainCar* simple, TrainCar* newCar){
+	if (simple == NULL){
+		//still a empty list
+		simple = newCar;
+	}
+	else{
+		//need to loop to the end of list because there is no "tail" in the class
+		TrainCar* itr = simple;
+		while (itr->next != NULL){
+			itr = itr->next;
+		}
+		newCar->prev = itr;
+		itr->next = newCar;
+	}
+	return;
+}
+
+void DeleteAllCars(TrainCar* simple){
+	
 	return;
 }
