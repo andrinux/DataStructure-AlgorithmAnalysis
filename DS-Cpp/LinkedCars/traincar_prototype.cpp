ed79a1cf4ac1d762dc98af5ce56e59b109eb1169
Andrinux Loid <andrinux.loid@gmail.com>
2015-05-06 23:03:06 -0400

    implement helper function of PushBack and Delete all the traincar

---
+++ DS-Cpp/LinkedCars/traincar_prototype.cpp
@@ -0,0 +1,10 @@
+// There are a number of additional functions used in main.cpp that
+// you need to declare and implement.
+
+//This file is used to implement some functions declared in traincar_prototytpe.h
+
+#include "traincar_prototypes.h"
+
+void PushBack(TrainCar* simple, static TrainCar* newCar){
+	return;
+}
