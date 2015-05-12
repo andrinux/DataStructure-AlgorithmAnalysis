fd49c20408f18607891e71d53a7bf0f0f6cb7c9c
Andrinux Loid <andrinux.loid@gmail.com>
2015-05-11 21:19:21 -0400

    member fuctions of class

--- DS-Cpp/TextQuery/TextQuery.h
+++ DS-Cpp/TextQuery/TextQuery.h
@@ -9,5 +9,26 @@
 #include <vector>
 #include <string>
 
+class TextQuery{
+public:
+	typedef std::vector<std::string>::size_type line_no;
+	/*iterfaces:
+	* read files: read the files and build the data structure of each line
+	* run_query find the given word and returns sets of lines which is appears
+	* text_line returns a line from the input file
+	*/
+	void read_file(std::ifstream &is) { store_file(is); build_map(); }
+	std::set<line_no> run_query(const std::string& str) const;
+	std::string text_line(line_no line) const;
+private:
+	//utility functions to read file
+	void store_file(std::ifstream& is);
+	//associate each word with a set of line numbers
+	void build_map();
+	//remember the whole input file
+	std::vector<std::string> lines_of_text;
+	//map word to set of the line on which it occurs
+	std::map<std::string, std::set<line_no> > word_map;
 
+};
 #endif
