2e7e56d3a433040a48edc01a246db636df95d904
Andrinux Loid <andrinux.loid@gmail.com>
2015-05-28 23:00:37 -0400

    unique character

--- Cracking/chap1/chap1_1.cpp
+++ Cracking/chap1/chap1_1.cpp
@@ -7,9 +7,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdint>
 
 //Search need to be efficient, ASCII value
-bool checkAllUnique(std::string str){
+bool checkAllUnique_1(std::string str){
 	int count[256] = {0};
 	for (int i = 0; i != str.size(); i++){
 		int cur = str[i];
@@ -21,16 +22,31 @@ bool checkAllUnique(std::string str){
 	return true;
 }
 
+bool checkAllUnique_2(std::string str){
+	uint32_t count[8] = { 0 };
+	for (int i = 0; i != str.size(); i++){
+		int cur = str[i];
+		int Byte = cur / 32; 
+		int Bit = 7 - cur % 32;
+		uint32_t mask = 0x01 << Bit;
+		if ((count[Byte] & mask) == 0)
+			count[Byte] |= count[Byte] | mask;
+		else
+			return false;
+	}
+
+	return true;
+}
+
 int main()
 {
 	std::vector<std::string> str;
-	std::vector<int> m(256);
 	str.push_back("Thequickbrownfoxjumpsoverthelazydog");
 	str.push_back("Hapy Life");
 	str.push_back("Good Day");
 	str.push_back("xyzabc");
 	str.push_back("A B C");
 	for (auto cur : str)
-		std::cout << checkAllUnique(cur) << " : " << cur << std::endl;
+		std::cout << checkAllUnique_1(cur) << "-"<<checkAllUnique_2(cur) << " : " << cur << std::endl;
 	return 1;
 }
