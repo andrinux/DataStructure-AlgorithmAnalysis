bd20631923739a79f7d60826e48c11c5b50bdf7f
Andrinux Loid <andrinux.loid@gmail.com>
2015-06-11 23:40:27 -0700

    the simplest runlength encoding

---
+++ Cracking/chap1/chap1_5_string_compress.cpp
@@ -0,0 +1,39 @@
+//Write code to compress a string
+//First version has bug
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+string strCompress(string& str){
+	string res;
+	char curChar=str[0];
+	int curLen = 1;
+	for (int i = 1; i != str.size(); i++){
+		if (curChar != str[i]){
+			//Save up
+			res += curChar;
+			res += ('0' + curLen); //important here
+			curChar = str[i];
+			curLen = 1;
+		}
+		else{
+			curLen++;
+		}
+	}
+	//Need to keep the last one
+	res += curChar;
+	res += ('0' + curLen); //important here
+	return res;
+}
+
+int main()
+{
+	string s1 = "aabcccccaaabbb";
+	string s2 = "aaaabbbbccddddsss";
+	cout << s1 << "-" << strCompress(s1) << endl;
+	cout << s2 << "-" << strCompress(s2) << endl;
+}
