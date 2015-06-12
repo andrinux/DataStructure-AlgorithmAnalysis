9486e9b1b0b3e4dcf7217d3d8bbcdb59d9b11ffa
Andrinux Loid <andrinux.loid@gmail.com>
2015-06-11 23:22:43 -0700

    replace a blank space to %20

--- Cracking/chap1/chap1_4_replace_space.cpp
+++ Cracking/chap1/chap1_4_replace_space.cpp
@@ -1,71 +1,44 @@
+//Write a method to replace all spaces in a string with ¡®%20¡¯.
+
 #include <iostream>
+#include <string>
 #include <cstring>
+#include <vector>
+
 using namespace std;
 
-char* replace1(char *c){
-	if (c == NULL) return NULL;
-	int len = strlen(c);
-	if (len == 0) return NULL;
-	int cnt = 0;
-	for (int i = 0; i<len; ++i)
-	{
-		if (c[i] == ' ')
-			++cnt;
-	}
-	char *cc = new char[len + 2 * cnt + 1];
-	int p = 0;
-	for (int i = 0; i<len; ++i)
-	{
-		if (c[i] == ' ')
-		{
-			cc[p] = '%';
-			cc[p + 1] = '2';
-			cc[p + 2] = '0';
-			p += 3;
-		}
-		else
-		{
-			cc[p] = c[i];
-			++p;
+#define MAX 100
+
+string replaceSpace(string s){
+	if (s.size() == 0)
+		return s;
+	char str[MAX] = { 0 };
+	int i = 0, j = 0;
+	for (i = 0; i != s.size(); i++){
+		if (s[i] != ' ')
+			str[j++] = s[i];
+		else{
+			str[j++] = '%';
+			str[j++] = '2';
+			str[j++] = '0';
 		}
 	}
-	cc[p] = '\0';
-	return cc;
+	str[j] = '\0';
+	string res(str);
+	return res;
 }
 
-void replace2(char *c){
-	if (c == NULL) return;
-	int len = strlen(c);
-	if (len == 0) return;
-	int cnt = 0;
-	for (int i = 0; i<len; ++i)
-	{
-		if (c[i] == ' ')
-			++cnt;
-	}
-	int p = len + 2 * cnt;
-	c[p--] = '\0';//the space must be allocated first.
-	for (int i = len - 1; i >= 0; --i)
-	{
-		if (c[i] == ' ')
-		{
-			c[p] = '0';
-			c[p - 1] = '2';
-			c[p - 2] = '%';
-			p -= 3;
-		}
-		else
-		{
-			c[p] = c[i];
-			--p;
-		}
-	}
+
+int main()
+{
+	string s0;
+	string s1 = "HelloWorld";
+	string s2 = "World Hello ";
+	string s4 = "    abc   ";
+	cout << s0 << "---"<< replaceSpace(s0) << endl;
+	cout << s1 << "---" << replaceSpace(s1) << endl;
+	cout << s2 << "---" << replaceSpace(s2) << endl;
+	cout << s4 << "---" << replaceSpace(s4) << endl;
+	return 1;
 }
-int main(){
-	const int len = 100;
-	char c[len] = "";
-	cout << replace1(c) << endl;
-	replace2(c);
-	cout << c << endl;
-	return 0;
-}
+
