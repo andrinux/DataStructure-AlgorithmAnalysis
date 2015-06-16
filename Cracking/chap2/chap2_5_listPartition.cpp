d691d8fa61f75bff9dbafa43959976e352e3b9ab
Andrinux Loid <andrinux.loid@gmail.com>
2015-06-15 23:05:08 -0700

    partition a list by value

--- Cracking/chap2/chap2_5_listPartition.cpp
+++ Cracking/chap2/chap2_5_listPartition.cpp
@@ -5,30 +5,85 @@
 
 #include <iostream>
 #include "myList.h"
+#include <vector>
 //Question: x is unique?
 
-//Version-1: Space O(n), time O(n)
+
+//Version-1: Space O(n), time O(2n)[because of an extra copy]
+//Not elegant at all.
 void partitionList_v1(myList& my, int x){
-	myList res1, res2;
-	Node *p1 = NULL;
-	Node *p = my.head;
+	std::vector<int> res1, res2, res3;
+	Node*p = my.head;
 	while (p != NULL){
-		if (p->value < x){
+		if (p->value < x)
 			res1.push_back(p->value);
-			if (p1 == NULL)
-				p1 = res1.head;
-			else
-				p1 = p1->next;
-		}
-		else{
+		if (p->value == x)
 			res2.push_back(p->value);
-		}
+		if (p->value> x)
+			res3.push_back(p->value);
 		p = p->next;
 	}
+	//Copy to 'my'
+	p = my.head;
+	for (int i = 0; i != res1.size(); i++){
+		p->value = res1[i];
+		p = p->next;
+	}
+	for (int i = 0; i != res2.size(); i++){
+		p->value = res2[i];
+		p = p->next;
+	}
+	for (int i = 0; i != res3.size(); i++){
+		p->value = res3[i];
+		p = p->next;
+	}
+
+}
+//Append a node to a list
+void append(Node* head, Node* cur){
+	if (head == NULL){
+		head = cur;
+		head->next = NULL;
+		return;
+	}
+	Node *tmp = head->next;
+	head->next = cur;
+	cur->next = tmp;
+	//std::cout << "Debug: head is " << head->value<< ", ";
+	//while (head != NULL){
+	//	std::cout << head->value<<"-";
+	//	head = head->next;
+	//}
+	//std::cout << std::endl;
+	return;
 }
 
 //version-2: Space O(1), time O(n^2)
+//Repartition in place
+//When met a large node, move it away to 'large list'
 void partitionList_v2(myList& my, int x){
+	Node *p = my.head;
+	Node* small = NULL;
+	Node* equal = NULL;
+	Node * large = NULL;
+	while (p != NULL){
+		Node* tmp = p->next;
+		if (p->value > x)
+			append(large, p);
+		if (p->value == x)
+			append(equal, p);
+		if (p->value < x)
+			append(small, p);
+		p = tmp;
+	}
+	my.head = small;
+	p = small;
+	while (p->next != NULL)
+		p = p->next;
+	p->next = equal;
+	while (p->next != NULL)
+		p = p->next;
+	p->next = large;
 
 }
 
@@ -43,9 +98,13 @@ int  main()
 	mylist.printList();
 	myList newlist(mylist);
 	partitionList_v1(mylist, 5);
+	std::cout << "Method-1: " << std::endl;
 	mylist.printList();
-	//Another
+	////Another
 	partitionList_v2(newlist, 5);
+	std::cout << "Method-2:" << std::endl;
 	newlist.printList();
+	//mylist.destroyList_Iter(mylist.head);
+	//mylist.printList();
 	return 1;
 }
