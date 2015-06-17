4c56d5b03fe7d877eaac62742359c46248a7fa4d
Andrinux Loid <andrinux.loid@gmail.com>
2015-06-16 23:09:32 -0700

    check Palindrome of single list

---
+++ Cracking/chap2/chap2_7_palindrome.cpp
@@ -0,0 +1,51 @@
+/*
+* Check if a list is a palindrome.
+*/
+
+#include <iostream>
+#include "myList.h"
+#include <stack>
+
+//Push the first half of list into stack
+//Pop and check. How to find the middle Node
+bool palindrome_v1(Node *& head){
+	std::stack<int> data;
+	Node *fast = head;
+	Node *slow = head;
+
+	while(fast!=NULL && fast->next != NULL){
+		data.push(slow->value);
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	if(fast!=NULL && fast->next == NULL)
+		slow=slow->next;
+
+	while(slow != NULL){
+		if(slow->value == data.top()){
+			data.pop();
+			slow = slow->next;
+		}else{
+			return false;
+		}
+	}
+	return true;
+}
+
+int  main()
+{
+	myList mylist;
+	mylist.push_back(1); mylist.push_back(6); mylist.push_back(2);
+	mylist.push_back(3); mylist.push_back(7); /*mylist.push_back(4);
+	mylist.push_back(4); */mylist.push_back(7); mylist.push_back(3);
+	mylist.push_back(2); mylist.push_back(6); mylist.push_back(1);
+	mylist.printList();
+	
+	bool res=palindrome_v1(mylist.head);
+	if(res)
+		std::cout << "Yes" << std::endl;
+	else
+		std::cout << "No" << std::endl;
+	
+	return 1;
+}
