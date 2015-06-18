70c14da6cc4d687299e3a8ab95fb0aa20288b477
Andrinux Loid <andrinux.loid@gmail.com>
2015-06-17 23:15:59 -0700

    3 stack use array, version-2

--- Cracking/chap3/chap3_1_StackUseArray.cpp
+++ Cracking/chap3/chap3_1_StackUseArray.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <iomanip>
 
+#define M 30
 //Version-1: Using an array, three stacktop pointers. 
 //The array is divided evenly into three parts.
 class Stack3_v1{
@@ -17,7 +18,7 @@ public:
 	Stack3_v1();
 	~Stack3_v1();
 	void push(int stackNo, int data);
-	void top(int stackNo);
+	int top(int stackNo);
 	void pop(int stackNo);
 	bool isEmpty(int stackNo);
 	void printStack(int stackNo);
@@ -25,7 +26,7 @@ public:
 
 //Default constructor
 Stack3_v1::Stack3_v1(){
-	totalsize = 900;
+	totalsize = M;
 	content = new int[totalsize];
 	stop[0] = -1;
 	stop[1] = -1;
@@ -36,25 +37,23 @@ Stack3_v1::~Stack3_v1(){
 	delete[] content;
 }
 
-//Stack1: 000->299
-//Stack2: 301->599
-//Stack3: 601->899
+//Push data into stack
 void Stack3_v1::push(int No, int data){
-	int max = 300 * (No + 1) - 1;
+	int max = M/3 - 1;
 	if (stop[No] == max){
-		std::cout << "Cannot Push: Stack" << No << "is FULL." << std::endl;
+		std::cout << "Cannot Push: Stack " << No << " is FULL." << std::endl;
 		return;
 	}
 	stop[No]++;
-	content[No*300 + stop[No]] = data;
+	content[No*(M/3) + stop[No]] = data;
 }
 
 void Stack3_v1::pop(int No){
 	if (stop[No] == -1){
-		std::cout << "Cannot Pop: Stack" << No << "is Empty." << std::endl;
+		std::cout << "Cannot Pop: Stack " << No << " is Empty." << std::endl;
 		return;
 	}
-	content[No*300 + stop[No]] = 0;
+	content[No*(M/3) + stop[No]] = 0;
 	stop[No]--;
 }
 
@@ -71,15 +70,32 @@ void Stack3_v1::printStack(int No){
 	if (stop[No] == -1)
 		return;
 	for (int i = 0; i != stop[No] + 1 ; i++){
-		std::cout << std::setw(3) << content[i + No * 300] << "->";
+		std::cout << std::setw(3) << content[i + No * (M / 3)] << "->";
 	}
 	std::cout << std::endl;
 }
 
+//Get the stack top value of one stack
+int Stack3_v1::top(int No){
+	return content[M / 3 * No + stop[No]];
+}
+
 //Version-2: The key Problem here is to avoid the case of "usage unbalance"
-//When one stack is used up. how to reallocate the space to minimize the collide
+//In this version, the three stacks are mixed, so we need a struct node to
+//store one data, and a 'cur' to indicate where can get the empty space.
+//Then the core array is an array of "node".
+class node{
+public:
+	int value;
+	int preIdx;
+public:
+	//Initializer expressions
+	node() : value(0), preIdx(-1) {}
+	node(int val, int pre) : value(val), preIdx(pre) {}
+};
+
 class Stack3_v2{
-	
+
 };
 
 int main() 
@@ -90,7 +106,15 @@ int main()
 		myStack.push(1, 3*i+1); 
 		myStack.push(2, 3*i+2);
 	}
+
 	myStack.printStack(1);
+	for (int i = 0; i != 11; i++){
+		myStack.pop(0);
+		myStack.pop(1);
+		myStack.pop(2);
+	}
+	myStack.printStack(1);
+
 	return EXIT_SUCCESS;
 }
 
