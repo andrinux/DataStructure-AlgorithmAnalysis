6662f5807c3286f18f9283af5bf87b2cc14cf701
iAndrinux <andrinux.loid@gmail.com>
2015-06-25 23:04:37 -0700

    find Path with a valye

--- Cracking/chap4/chap4_9_sumPath.cpp
+++ Cracking/chap4/chap4_9_sumPath.cpp
@@ -4,3 +4,42 @@
 * have to start at the root.
 */
 
+//path may be not unique, so try to find all of them.
+
+#include <iostream>
+#include "BinaryTree.h"
+
+Node* ArrayToTree(int a[], Node * parent, int start, int end){
+	//base case.
+	if (start > end)
+		return NULL;
+	BinaryTree * BST = new BinaryTree();
+	int mid = (start + end) / 2;
+	//general case
+	BST->root = new Node(a[mid]);
+	BST->root->parent = parent;
+	BST->root->left = ArrayToTree(a, BST->root, start, mid - 1);
+	BST->root->right = ArrayToTree(a, BST->root, mid + 1, end);
+	return BST->root;
+}
+
+//Find a path summing to a given value, with parent node
+void PathFinder(int value, Node * root){
+
+}
+
+//Second Version without parent node in the 'Node'
+void PathFinder_2(int value, Node *root){
+}
+
+
+int main()
+{
+    int D[22] = {0, 4, 5, 7, 3, 1, 2, 7, 9, 11, 5, 14, 11, 10, 9, 8, 15, 6, 12, 13, 20, 17};
+	BinaryTree T;
+	T.root = ArrayToTree(D, NULL, 0, 21);
+	T.print();
+
+    return 0;
+}
+
