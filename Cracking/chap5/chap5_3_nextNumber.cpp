749c1574a3100a65c6d5c887ca42fba00192a7e1
Andrinux Loid <andrinux.loid@gmail.com>
2015-06-27 23:46:51 -0700

    next number with same amount '1'

---
+++ Cracking/chap5/chap5_3_nextNumber.cpp
@@ -0,0 +1,19 @@
+/*
+* Given an integer, print the next smallest and next largest number that have the 
+* same number of 1 bits in their binary representation.
+*/
+
+//Tricky, Not finished yet.
+
+#include <iostream>
+
+int nextNum(int M){
+
+}
+
+int main()
+{
+	int M = 9;
+	int N = nextNum(M);
+	std::cout << N << std::endl;
+}
