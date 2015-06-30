e518369143616e75caf3b0dbc04cfd3c386d3b31
Andrinux Loid <andrinux.loid@gmail.com>
2015-06-29 19:08:50 -0700

    fix bug of merge sort

--- Cracking/chap9/mergesort.cpp
+++ Cracking/chap9/mergesort.cpp
@@ -11,23 +11,18 @@
 #define M 100
 using namespace std;
 
-void swap(vector<int> & data, int i, int j){
-	int tmp = data[i];
-	data[i] = data[j];
-	data[j] = tmp;
-}
 
 //In-place merge.
 void do_merge(vector<int> &data, int s, int e)
 {
 	vector<int> sorted;
 	int i = s;
-	int j = s + (e - s) / 2 + 1;
-	while (sorted.size() <= (e - s + 1)){
-		if (data[i] > data[j]){
+	int j = (s+e)/2 + 1;
+	while (sorted.size() < (e - s + 1)){
+		if (data[i] < data[j]){
 			sorted.push_back(data[i]);
 			i++;
-			if (i == s + (e - s) / 2){
+			if (i > (e + s) / 2){
 				for (; j <= e; j++)
 					sorted.push_back(data[j]);
 			}
@@ -36,8 +31,8 @@ void do_merge(vector<int> &data, int s, int e)
 			//data[i] <= data[j]
 			sorted.push_back(data[j]);
 			j++;
-			if (j == e){
-				for (; i <= s + (e - s) / 2; i++)
+			if (j > e){
+				for (; i <=  (e + s) / 2; i++)
 					sorted.push_back(data[i]);
 			}
 		}
@@ -65,20 +60,20 @@ int main()
 	srand((unsigned) time(NULL));
 	vector<int> data;
 	for (int i = 0; i != M; i++)
-		data.push_back(rand() % 500);
-	for (int i = 0; i != M; i++){
-		cout << setw(3) << data[i] << " -> ";
-		if ((i + 1) % 9 == 0)
-			std::cout << std::endl;
-	}
-	std::cout << "\n--------------------\n";
+		data.push_back(rand() % 1000);
+	//for (int i = 0; i != M; i++){
+	//	cout << setw(3) << data[i] << " -> ";
+	//	if ((i + 1) % 10 == 0)
+	//		std::cout << std::endl;
+	//}
+	//std::cout << "\n--------------------\n";
 
-	mergeSortR(data, 0, M-1);
+	mergeSortR(data, 0, data.size()-1);
 	
-	for (int i = 0; i != M; i++){
-		cout << setw(3) << data[i] << " -> ";
-		if ((i + 1) % 9 == 0)
-			std::cout << std::endl;
-	}
+	//for (int i = 0; i != M; i++){
+	//	cout << setw(3) << data[i] << " -> ";
+	//	if ((i + 1) % 10 == 0)
+	//		std::cout << std::endl;
+	//}
 	return 0;
 }
