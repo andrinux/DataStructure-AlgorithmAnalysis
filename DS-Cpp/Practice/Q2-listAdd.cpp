8447380713d832c10da5d25714e3f89443ea29f7
Andrinux Loid <andrinux.loid@gmail.com>
2015-07-12 19:27:00 -0700

    add two list

---
+++ DS-Cpp/Practice/Q2-listAdd.cpp
@@ -0,0 +1,11 @@
+//Assume two large numbers are stored in two list
+//DO I need to write the list structure myself here? 
+
+#include "mylist.h"
+#include <iostream>
+
+int main()
+{
+
+	return 1;
+}
