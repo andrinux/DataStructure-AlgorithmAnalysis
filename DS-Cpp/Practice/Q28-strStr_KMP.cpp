ceac106b7e20ef9e52888e7373bf1152366568a1
iAndrinux <andrinux.loid@gmail.com>
2015-09-27 00:01:18 -0400

    the KMP method: important

--- DS-Cpp/Practice/Q28-strStr_KMP.cpp
+++ DS-Cpp/Practice/Q28-strStr_KMP.cpp
@@ -8,7 +8,49 @@ using namespace std;
 
 class Solution {
 public:
+	/*
+	*    the K-M-P algorithm for string matching
+	*    Complexity is O(m+n)
+	*/
+	void getNext(string p, vector<int> & next){
+		next[0] = -1;
+		int j = 0, k = -1;
+		while (j < p.size()-1){
+			if (k == -1 || p[j] == p[k]){
+				j++;
+				k++;
+				next[j] = k;
+			}
+			else
+				k = next[k];
+		}		
+	}
 
+	int strStr(string str, string pattern) {
+		if (pattern.size() == 0)
+			return 0;
+		if (str.size() == 0)
+			return -1;
+		int i = 0, j = 0;
+		int M = str.size(), N = pattern.size(); // MUST use int here., or will cause bug in the loop.
+		vector<int> Next(pattern.size(), 0);
+		//pre-process
+		getNext(pattern, Next);
+		//search.
+		while (i < M && j < N){
+			if (j == -1 || str[i] == pattern[j]){
+				i++;
+				j++;
+			}
+			else{
+				j = Next[j];
+			}
+		}
+		if (j == pattern.size())
+			return i - j;
+		else
+			return -1;
+	}
 
 	/*
 	*    the Brute Force Method of string matching.
