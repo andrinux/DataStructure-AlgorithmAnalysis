fd7c7d93f8392b55e78c5db4401580a98dff44b1
iAndrinux <andrinux.loid@gmail.com>
2015-10-06 00:02:30 -0400

    O(n) AC code

--- DS-Cpp/Practice/Q45_JumpGame_II.cpp
+++ DS-Cpp/Practice/Q45_JumpGame_II.cpp
@@ -21,7 +21,24 @@ public:
 	}
 };
 
-
+// A cleaner method.
+class Solution {
+public:
+	int jump(vector<int>& nums) {
+		int step = 0, curMax = 0, lastMax = 0;
+		int i = 0;
+		int N = nums.size();
+		while (curMax < N - 1){
+			lastMax = curMax;
+			while (i <= lastMax){
+				curMax = max(curMax, i + nums[i]);
+				i++;
+			}
+			step++;
+		}
+		return step;
+	}
+};
 
 int main()
 {
