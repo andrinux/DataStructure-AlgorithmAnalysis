f2c32d78a4045572a05079f83859fb0dd7e37b0b
iAndrinux <andrinux.loid@gmail.com>
2015-10-09 00:00:15 -0400

    DP: find the max sub arrat

---
+++ DS-Cpp/Practice/Q53_MaximumSubarray.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//One way DP Method.
+class Solution_1 {
+public:
+	int maxSubArray(vector<int>& nums) {
+		int M = nums.size();
+		//max value will be negative, so we need to intialize it as INT_MIN
+		int curSum = 0, maxSum = INT_MIN;
+		for (int i = 0; i < M; i++){
+			curSum = curSum + nums[i];
+			if (curSum > maxSum)
+				maxSum = curSum;
+			if (curSum <= 0)
+				curSum = 0;
+		}
+		return maxSum;
+	}
+};
+
+//Divide and Conquer Method.
+class Solution {
+public:
+	int maxSubArray(vector<int>& nums) {
+
+	}
+};
+
+int main()
+{
+	vector<int> nums = { -2, -1};
+	Solution so;
+	cout << so.maxSubArray(nums) << endl;
+	return 0;
+}
