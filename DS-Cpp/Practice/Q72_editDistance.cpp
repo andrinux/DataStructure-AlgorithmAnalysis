c8b076661f2090c103d7962bed67580ac235bea5
iAndrinux <andrinux.loid@gmail.com>
2015-10-20 00:00:22 -0400

    not finish yet

---
+++ DS-Cpp/Practice/Q72_editDistance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	int minDistance(string w1, string w2) {
+		return 0;
+	}
+};
+
+int main()
+{
+	Solution so;
+	string w1, w2;
+	w1 = "dog"; w2 = "cat"; cout <<w1 <<"-"<<w2 <<" " << so.minDistance(w1, w2) << endl;
+	return 0;
+}
